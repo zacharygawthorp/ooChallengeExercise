2b6d1c0330fbc8c58a7e0fbd35dcb1226bedf68f
zacharygawthorp <107508857+zacharygawthorp@users.noreply.github.com>
2022-06-22 14:47:47 -0500

    Add files via upload

---
+++ ooChallenge.js
@@ -0,0 +1,47 @@
+class Vehicle {
+    constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    }
+    honk() {
+        return "Beep";
+    }
+    toString() {
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}`
+    }
+}
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 4;
+    }        
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+    revEngine() {
+        return "VROOM!!!"
+    }
+}
+
+class Garage {
+    constructor(capacity) {
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+    add(newVehicle) {
+        if(!(newVehicle instanceof Vehicle)) {
+            return "Only vehicles are allowed!";
+        }
+        if(this.vehicles.length >= this.capacity) {
+            return "Sorry, we are full.";
+        }
+        this.vehicles.push(newVehicle);
+        return "You added a Vehicle to the Garage!!";
+    }
+}
